f5412914a1aff0c6c03256d39b39c33b56c6aa85
Aurelien <aurelien.rebourg@gmail.com>
2018-12-31 18:31:59 +0100

    bon bah ca marche

--- src/LineChart.js
+++ src/LineChart.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import * as Pixi from 'pixi.js'
 import { Stage, Graphics } from '@inlet/react-pixi'
-import Traveller from './Traveller'
+
 
 class LineChart extends Component {
     app: Pixi.Application
@@ -36,6 +37,94 @@ class LineChart extends Component {
         this.setupLineChart()
         this.setupTraveller()
         this.app.render(this.stage)*/
+    }
+
+    setupTraveller() {
+        this.traveller.interactive = true
+        this.traveller.on('mousedown', event => this.onDragStart(event))
+                        .on('touchstart', event => this.onDragStart(event))
+                        .on('mouseup', () => this.onDragEnd())
+                        .on('mouseupoutside', () => this.onDragEnd())
+                        .on('touchend', () => this.onDragEnd())
+                        .on('touchendoutside', () => this.onDragEnd())
+                        .on('mousemove', () => this.onMouseMove())
+                        .on('touchmove', () => this.onMouseMove());
+
+        this.lineChart(0, this.props.height * 5/6, this.props.width, this.props.height * 1/6, this.state.min_data_x, 1700123529, [this.props.lines[0], ])
+        this.drawTraveller(0, this.props.height * 5/6, this.props.width, this.props.height * 1/6, this.state.display_min_x, this.state.display_max_x)
+    }
+
+    onDragStart(event) {
+        let multip = this.props.width / Math.abs(this.state.max_data_x - this.state.min_data_x)
+        this.setState({
+            data: event.data,
+            dragging: true,
+            initial_spread: this.state.display_max_x - this.state.display_min_x,
+            initial_cursor_pos: event.data.getLocalPosition(this.traveller).x - multip * Math.abs(this.state.min_data_x - this.state.display_min_x)
+        })
+
+        let newPosition = event.data.getLocalPosition(this.traveller).x
+        let right_side_pos = multip * Math.abs(this.state.min_data_x - this.state.min_data_x + this.state.initial_spread)
+        let dragging_side = ""
+        console.log(this.state.initial_cursor_pos, right_side_pos)
+        if(this.state.initial_cursor_pos < 25) {
+            dragging_side = "left"
+        } else if(this.state.initial_cursor_pos + 25 > right_side_pos) {
+            console.log("RIGHT!!:")
+            dragging_side = "right"
+        } else {
+            dragging_side = "middle"
+        }
+        this.setState({dragging_side: dragging_side})
+    }
+
+    onDragEnd() {
+        this.setState({data: null, dragging: false})
+        this.lines.clear()
+        this.lineChart(0, 0, this.props.width, this.props.height * 5/6, this.state.display_min_x, this.state.display_max_x, this.props.lines)
+        this.lineChart(0, this.props.height * 5/6, this.props.width, this.props.height * 1/6, this.state.min_data_x, 1700123529, [this.props.lines[0], ])
+    }
+
+    onMouseMove() {
+        console.log("move!")
+        if (this.state.dragging) {
+            let newPosition = this.state.data.getLocalPosition(this.traveller).x
+            if(newPosition - this.state.initial_cursor_pos > 0) {
+                newPosition -= this.state.initial_cursor_pos
+            } else {
+                newPosition = 0
+            }
+            console.log(newPosition)
+            let multip = this.props.width / Math.abs(this.state.max_data_x - this.state.min_data_x)
+            switch(this.state.dragging_side) {
+                case "left":
+                    this.setState({display_min_x: this.state.min_data_x + newPosition / multip })
+                    break
+                case "right":
+                    this.setState({display_max_x: this.state.min_data_x + this.state.data.getLocalPosition(this.traveller).x / multip })
+                    break
+                case "middle":
+                    this.setState({display_min_x: this.state.min_data_x + newPosition / multip })
+                    this.setState({display_max_x: this.state.min_data_x + newPosition / multip + this.state.initial_spread })
+                    break
+            }
+        //    multip * Math.abs(this.state.min_data_x - to_x)
+            this.traveller.clear()
+            this.drawTraveller(0, this.props.height * 5/6, this.props.width, this.props.height * 1/6, this.state.display_min_x, this.state.display_max_x)
+        }
+    }
+
+
+    drawTraveller = (x, y, width, height, from_x, to_x) => {
+        this.traveller.beginFill(0xAAAAAA, 0.1);
+
+        this.traveller.lineStyle(3, 0x000000);
+        let multip = width / Math.abs(this.state.max_data_x - this.state.min_data_x)
+        // draw a rectangle
+        this.traveller.drawRect(multip * Math.abs(this.state.min_data_x - from_x), y, multip * Math.abs(from_x - to_x), height);
+        //his.traveller.beginFill(0x000000, 1);
+        this.traveller.drawRect(multip * Math.abs(this.state.min_data_x - from_x), y, 25, height);
+        this.traveller.drawRect(multip * Math.abs(this.state.min_data_x - to_x), y, 25, height);
     }
 
     lineChart = (pos_x, pos_y, width, height, from_x, to_x, lines) => {
@@ -106,13 +195,11 @@ class LineChart extends Component {
                         this.lineChart(0, 0, this.props.width, this.props.height * 5/6, this.state.display_min_x, this.state.display_max_x, this.props.lines)
                     }}
                 />
-                <Traveller
-                    width={this.props.width}
-                    height={this.props.height}
-                    state={this.state}
-                    lines={this.props.lines}
-                    lineChart={this.lineChart}
-                    wrapperEnd={() => this.lines.clear()}
+                <Graphics
+                    draw={traveller => {
+                        this.traveller = traveller
+                        this.setupTraveller()
+                    }}
                 />
             </Stage>
         );
